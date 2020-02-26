cad5bb5c274291434df638d63006455c20c63687
fengzelin.isumi <fengzelin.isumi@bytedance.com>
2020-02-26 08:43:58 +0800

    add linkButtonDynAttr
    Change-Id: I8519dba176067d8f5015f05f62e34665f25c9306

--- src/Reflex/Dom/Bulma/Component/Button.hs
+++ src/Reflex/Dom/Bulma/Component/Button.hs
@@ -1,5 +1,7 @@
 module Reflex.Dom.Bulma.Component.Button
   ( linkButton
+  , linkButtonDynAttr
+  , linkButtonDynAttr_
   , buttonAttr
   , buttonDynAttr
   ) where
@@ -15,6 +17,22 @@ linkButton :: MonadWidget t m
 linkButton attr child = do
     (e, a) <- elAttr' "a" attr child
     pure (domEvent Click e, a)
+
+linkButtonDynAttr :: MonadWidget t m
+                  => Dynamic t (Map Text Text)
+                  -> m a
+                  -> m (Event t (), a)
+linkButtonDynAttr attrDyn child = do
+    (e, a) <- elDynAttr' "a" attrDyn child
+    pure (domEvent Click e, a)
+
+linkButtonDynAttr_ :: MonadWidget t m
+                   => Dynamic t (Map Text Text)
+                   -> m a
+                   -> m (Event t ())
+linkButtonDynAttr_ attrDyn child = do
+    (e, a) <- linkButtonDynAttr attrDyn child
+    pure e
 
 buttonAttr :: MonadWidget t m
            => Map Text Text
